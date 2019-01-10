acb4d3fd6cb5c7e9a3e3191a8ead91a6d97a6998
yh_victor <yhvictor@users.noreply.github.com>
2019-01-10 11:18:24 -0800

    Add how to

--- default.generate.config.sh
+++ default.generate.config.sh
@@ -1,3 +1,6 @@
+# Copy this config to generate.config.sh
+# and change values.
+
 base_href=experiment/yhvictor
 client_repo=~/IdeaProjects/bbuhot
 github_page_repo=~/github/SingerofDarkNight.github.io
