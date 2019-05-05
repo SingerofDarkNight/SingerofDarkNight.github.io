789803c439e87958b50c5ba9547f74857b080fac
yh_victor <yhvictor@users.noreply.github.com>
2019-05-05 05:22:59 -0700

    Upload again.

--- default.generate.config.sh
+++
@@ -1,6 +0,0 @@
-# Copy this config to generate.config.sh
-# and change values.
-
-base_href=experiment/yhvictor
-client_repo=~/IdeaProjects/bbuhot
-github_page_repo=~/github/SingerofDarkNight.github.io
